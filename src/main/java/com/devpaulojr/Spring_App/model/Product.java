ff8becb9a83795a38ccf58d41c4e2dac238ccf21
devpaulojr <paulosantanajunior133@gmail.com>
2024-12-12 13:12:34 -0300

    ManyToMany associado com product e orderItem

--- src/main/java/com/devpaulojr/Spring_App/model/Product.java
+++ src/main/java/com/devpaulojr/Spring_App/model/Product.java
@@ -1,13 +1,7 @@
 package com.devpaulojr.Spring_App.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.HashSet;
@@ -32,6 +26,9 @@ public class Product implements Serializable {
             joinColumns = @JoinColumn(name = "fk_product"),
             inverseJoinColumns = @JoinColumn(name = "fk_category"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product(){
 
@@ -87,6 +84,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories(){
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for(OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
